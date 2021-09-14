7fa8aecaa3a8497f54641628654bc060659da837
Olga Shevchuk <morozolga92@gmail.com>
2021-09-15 00:09:42 +0300

    optimise import

--- src/test/java/pages/workspace/MembersPage.java
+++ src/test/java/pages/workspace/MembersPage.java
@@ -3,13 +3,14 @@ package pages.workspace;
 import baseEntities.BasePage;
 import com.codeborne.selenide.SelenideElement;
 import core.ReadProperties;
+import pages.UpgradeSubscriptionPage;
 
 import static com.codeborne.selenide.Selenide.$;
 import static com.codeborne.selenide.Selenide.open;
 
-public class WorkspacePage extends BasePage {
+public class MembersPage extends BasePage {
 
-    public WorkspacePage(boolean openPageByUrl, String endpoint) {
+    public MembersPage(boolean openPageByUrl, String endpoint) {
         super(openPageByUrl, endpoint);
     }
 
@@ -23,17 +24,9 @@ public class WorkspacePage extends BasePage {
         return $("a[title='Custom']");
     }
 
-    public SelenideElement getUpgradeSubscriptionBtn() {
-        return $(".btn.btn-primary");
-    }
-
-    public SelenideElement getUpgradeSubscriptionHeader() {
-        return $(".modal-content .header");
-    }
-
-    public WorkspacePage clickCustomFieldsMenuItemUpgrade() {
+    public UpgradeSubscriptionPage clickCustomFieldsMenuItemUpgrade() {
 
         getCustomFieldsMenuItem().click();
-        return this;
+        return new UpgradeSubscriptionPage();
     }
 }

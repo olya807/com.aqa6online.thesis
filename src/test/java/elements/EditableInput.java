2d9f930ecf170c28c64b36741ce0090f1632811c
Olga Shevchuk <morozolga92@gmail.com>
2021-09-16 01:09:49 +0300

    refactor

--- src/test/java/elements/EditableInput.java
+++ src/test/java/elements/EditableInput.java
@@ -8,20 +8,19 @@ import static com.codeborne.selenide.Selenide.$x;
 public class EditableInput {
 
     String label;
-    String field = "//div[@id='undefinedGroup'][label[text()='%s']]//p";
+    String inputField = "//div[@id='undefinedGroup'][label[text()='%s']]//p";
 
     public EditableInput(String label) {
         this.label = label;
     }
 
     public void insert(String text) {
-        SelenideElement inputField = $x(String.format(field, label));
+        SelenideElement inputField = $x(String.format(this.inputField, label));
         WebDriverRunner.driver().executeJavaScript("arguments[0].setAttribute('value', arguments[1])", inputField, text);
-        //$x(String.format(field, label)).setValue(text);
     }
 
     public EditableInput clear() {
-        $x(String.format(field, label)).clear();
+        $x(String.format(inputField, label)).clear();
         return this;
     }
 }

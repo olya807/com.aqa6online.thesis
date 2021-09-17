f6e261e252b0ae5dce536fa1c9f60cc4cd03d123
Olga Shevchuk <morozolga92@gmail.com>
2021-09-17 23:33:25 +0300

    temp

--- src/test/java/tests/api/DeleteSuiteProject.java
+++ src/test/java/tests/api/DeleteSuiteProject.java
@@ -3,15 +3,34 @@ package tests.api;
 import adapters.ProjectsAdapter;
 import adapters.SuitesAdapter;
 import baseEntities.BaseApiTest;
+import com.codeborne.selenide.Condition;
+import io.qameta.allure.Description;
 import models.projectModels.GetResponseResult;
 import models.projectModels.PostResponseResult;
 import models.suitesModels.SuiteResponseResult;
+import org.apache.commons.lang3.RandomStringUtils;
 import org.testng.Assert;
 import org.testng.annotations.Test;
+import steps.LoginStep;
 
 public class DeleteSuiteProject extends BaseApiTest {
 
     protected int suiteId;
+
+    @Test
+    @Description("Login with incorrect 'Email' field value")
+    public void loginWithIncorrectEmailFieldValue() {
+
+        String loginErrorMessage = "These credentials do not match our records.";
+        String incorrectLogin = RandomStringUtils.randomAlphanumeric(7) + "@" + RandomStringUtils.randomAlphabetic(5) + ".com";
+        String incorrectPSW = RandomStringUtils.randomAlphanumeric(8);
+
+        new LoginStep()
+                .incorrectLogin(incorrectLogin, incorrectPSW)
+                .getCredentialsDoNotMatchMessage()
+                .shouldBe(Condition.visible)
+                .shouldHave(Condition.exactText(loginErrorMessage));
+    }
 
     @Test
     public void createProjectsTest(){

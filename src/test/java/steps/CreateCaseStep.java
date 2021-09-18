34b5ddc96c62435a13fce245540319d86a7ea12b
quality100 <quality100@yandex.ru>
2021-09-18 17:05:03 +0300

    god save the queen

--- src/test/java/steps/CreateCaseStep.java
+++ src/test/java/steps/CreateCaseStep.java
@@ -1,21 +1,24 @@
 package steps;
 
 import endpoints.UiEndpoints;
+import io.qameta.allure.Step;
 import models.TestCase;
 import pages.CasePage;
 import pages.ProjectPage;
 
 public class CreateCaseStep {
 
-    public ProjectPage createCase(String randomProjectCode, TestCase testCase){
+    @Step
+    public ProjectPage createCase(String randomProjectCode, TestCase testCase) {
         CasePage casePage = new CasePage(true, String.format(UiEndpoints.CASE_CREATE, randomProjectCode));
         casePage
                 .fillCaseForm(testCase)
                 .clickSaveButton();
-        return new ProjectPage(false,String.format(UiEndpoints.PROJECT,randomProjectCode));
+        return new ProjectPage(false, String.format(UiEndpoints.PROJECT, randomProjectCode));
     }
 
-    public CasePage createCaseWithTitleOnly(String randomProjectCode, TestCase testCase){
+    @Step
+    public CasePage createCaseWithTitleOnly(String randomProjectCode, TestCase testCase) {
         CasePage casePage = new CasePage(true, String.format(UiEndpoints.CASE_CREATE, randomProjectCode));
         casePage
                 .fillCaseFormWithTitleOnly(testCase)
